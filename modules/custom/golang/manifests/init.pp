77f92f926851c82e95126a517dbcfe2078675c52
vaequitas <thomas@flack.it>
2018-02-25 18:23:11 +0000

    Add check for osfamily in golang module

--- modules/custom/golang/manifests/init.pp
+++ modules/custom/golang/manifests/init.pp
@@ -1,8 +1,15 @@
 class golang (
   Optional[Enum['present', 'absent']] $ensure = $::golang::params::ensure
 ) inherits ::golang::params {
-  package {'golang':
-    ensure   => $ensure,
-    provider => 'yum',
+  case $osfamily {
+    'CentOS', 'RedHat': {
+      package { 'golang':
+        ensure   => $ensure,
+        provider => 'yum',
+      }
+    }
+    default: {
+      fail "Module `${module_name}` does not currently support OSFamily `${osfamily}`."
+    }
   }
 }
